93a2e1ad9ce1f15f633100c6db5d368624ce99ef
Bruno Miranda <bruno@bopia.com>
2008-04-30 17:52:01 -0400

    Added initial rails app.

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '20f1f495d10868e7b22140c5a4f11a4d'
+end
