5395e5f9ae93fac6110b0d200e16b99ed09a3c53
Bruno Miranda <bruno@bopia.com>
2008-05-07 12:14:31 -0400

    Adding necessary deployment files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,19 +2,21 @@
 set :application, "rubyonda"
 set :domain,      "70.85.16.206"
 set :deploy_to,   "/var/rails/#{application}"
+set :scm,         "git"
 set :repository,  "git://github.com/brupm/onda.git"
 
+
 set :mongrel_port, 8070
-set :mongrel_servers, 3
+set :mongrel_servers, 2
 
 namespace :vlad do
   desc 'Runs vlad:update, vlad:symlink, vlad:migrate and vlad:start'
   task :deploy => ['vlad:update', 'vlad:symlink', 'vlad:migrate', 'vlad:stop_app', 'vlad:start_app']
 
-  desc 'Symlinks your custom directories'
-  remote_task :symlink, :roles => :app do
-    run "ln -s #{shared_path}/assets #{latest_release}/public/assets" 
-  end
+  #desc 'Symlinks your custom directories'
+  #remote_task :symlink, :roles => :app do
+  #  run "ln -s #{shared_path}/assets #{latest_release}/public/assets" 
+  #end
 
   #desc 'Setup your custom directories in shared.'
   #remote_task :setup_shared, :roles => :app do
