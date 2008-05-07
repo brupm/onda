988a540d51a3715c234e8c4efb538a30634e2834
Roberto Soares <roberto.tech@gmail.com>
2008-05-07 00:53:16 -0300

    fix: already initialized constant ERROR_MESSAGES

--- config/initializers/conventions.rb
+++ config/initializers/conventions.rb
@@ -1,9 +1,8 @@
 WillPaginate::ViewHelpers.pagination_options[:prev_label] = '&laquo;  Página Anterior'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Próxima Página &raquo;'
 
-OpenIdAuthentication::Result::ERROR_MESSAGES = {
+OpenIdAuthentication::Result::ERROR_MESSAGES.merge(
       :missing      => "Servidor OpenID não encontrado",
       :canceled     => "A verificação do OpenID foi cancelada",
       :failed       => "A verificação do OpenID falhou",
-      :setup_needed => "A verificação do OpenID precisa ser configurada"
-    }
+      :setup_needed => "A verificação do OpenID precisa ser configurada")
