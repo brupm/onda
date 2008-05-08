9f9481040fe3464923d3660094f44c49b65ca9c8
Roberto Soares <roberto.tech@gmail.com>
2008-05-08 16:42:43 -0300

    tweet em 5 minutos apos publicado

--- lib/tweet_posts.rb
+++ lib/tweet_posts.rb
@@ -7,7 +7,7 @@ class TweetPosts
   def self.execute
     twit = Twitter::Base.new(ENV['TWITTER_USER'], ENV['TWITTER_PASS'])
 
-    Post.find_latest(:limit => 50, :order => "published_at", :conditions => ["published_at < DATE_SUB(?, INTERVAL 15 MINUTE) AND tweeted_at IS NULL", Time.now.utc]).each do |post|
+    Post.find_latest(:limit => 50, :order => "published_at", :conditions => ["published_at < DATE_SUB(?, INTERVAL 5 MINUTE) AND tweeted_at IS NULL", Time.now.utc]).each do |post|
       max_size = 140 - post.full_url.chars.length - 1 #(space)
       max_size_truncate = max_size - "...".chars.length
       title = post.title.strip
