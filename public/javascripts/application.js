3f1e9496048272393dfc545e7fce38dec74a7ab5
Roberto Soares <roberto.tech@gmail.com>
2008-05-13 14:21:03 -0300

    help: apenas no index e com position absolute

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,13 @@
 window.setTimeout('showHelp()',5000);
 function showHelp() {
-  document.getElementById("help").style.display = "block";      
+  if (document.getElementById("help")) {
+    document.getElementById("help").style.display = "block";
+  }
 }
 window.setTimeout('hideHelp()',50000);
 function hideHelp() {
-  document.getElementById("help").style.display = "none";      
+  if (document.getElementById("help")) {
+    document.getElementById("help").style.display = "none";
+  }
 }
-window.clearTimeout()
+window.clearTimeout();
