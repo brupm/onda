8c46d2f54267b44d884def7d7aa71f124c572e80
Roberto Soares <roberto.tech@gmail.com>
2008-05-15 21:34:25 -0300

    fix bot: 'enviador por' -> 'enviado por'

--- lib/tweet_posts.rb
+++ lib/tweet_posts.rb
@@ -8,8 +8,8 @@ class TweetPosts
     twit = Twitter::Base.new(ENV['TWITTER_USER'], ENV['TWITTER_PASS'])
 
     Post.find_latest(:limit => 50, :order => "published_at", :conditions => ["published_at < DATE_SUB(?, INTERVAL 5 MINUTE) AND tweeted_at IS NULL", Time.now.utc]).each do |post|
-      signature = " enviador por " + (post.user.twitter_user.blank? ? "#{post.user.nick}" : "@#{post.user.twitter_user}") 
-      max_size = 140 - signature.chars.length - 35 - 1 #(tinyurl: 35, space: 1)
+      signature = " enviado por " + (post.user.twitter_user.blank? ? "#{post.user.nick}" : "@#{post.user.twitter_user}") 
+      max_size = 140 - signature.chars.length - 35 - 1 #(tinyurl: +-35, space: 1)
       max_size_truncate = max_size - "...".chars.length
       title = post.title.strip
       title = (title.chars.length > max_size ? title.chars[0...max_size_truncate] + "..." : title).to_s
