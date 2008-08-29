2527f4f3c55c05469923224f80156c19aec920c9
Bruno Miranda <bruno@bopia.com>
2008-08-29 16:02:48 -0400

    Small changes to widget and about screen

--- vendor/plugins/custom_resource_name/init.rb
+++
@@ -1,8 +0,0 @@
-require 'custom_resource_name'
-
-if Rails::VERSION::MAJOR < 2
-  require 'extract_options'
-  require 'map_associations'
-end
-
-require 'resources' if RAILS_GEM_VERSION < '1.2.6'
--- vendor/plugins/sitemap/app/controllers/sitemap_plugin_controller.rb
+++
@@ -1,9 +0,0 @@
-class SitemapPluginController < ActionController::Base
-  layout 'sitemap_plugin'
-  before_filter :check_sitemap_auth
-  
-  def check_sitemap_auth
-
-  end
-  
-end
