2f5e44ea3fc8033dda04048c2e4ea2c8e4b87811
Bruno Miranda <bruno@bopia.com>
2008-10-27 13:38:14 -0400

    Removing rails summit banner.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,6 +1,6 @@
 class Notifier < ActionMailer::Base
   def new_post(post)
-    recipients  ["bruno@bopia.com", "roberto@bopia.com", post.user.email].compact
+    recipients  ["roberto@bopia.com", post.user.email].compact
 #    recipients User.find(:all, :conditions => {:admin => true}).collect(&:email).join(' ')
     from        "rubyonda@bopia.com"
     subject     "Novo artigo em RubyOnda: #{post.state_pt}"
