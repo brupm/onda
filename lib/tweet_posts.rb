013af024fa830b5ed95b775b5191299a5a4f5d15
Davis Zanetti Cabral <daviscabral@gmail.com>
2009-08-04 20:18:15 -0300

    Update instead post (post aren't available since July,15 2009

--- lib/tweet_posts.rb
+++ lib/tweet_posts.rb
@@ -8,13 +8,15 @@ class TweetPosts
     httpauth = Twitter::HTTPAuth.new(ENV['TWITTER_USER'], ENV['TWITTER_PASS'])
     twit = Twitter::Base.new(httpauth)
 
-    Post.find_latest(:limit => 50, :order => "published_at", :conditions => ["published_at < DATE_SUB(?, INTERVAL 5 MINUTE) AND tweeted_at IS NULL", Time.now.utc]).each do |post|
+    posts = Post.find_latest(:limit => 50, :order => "published_at", :conditions => ["(1=1 OR published_at < DATE_SUB(?, INTERVAL 5 MINUTE)) AND tweeted_at IS NULL", Time.now])
+    
+    posts.each do |post|
       signature = " enviado por " + (post.user.twitter_user.blank? ? "#{post.user.nick}" : "@#{post.user.twitter_user}") 
       max_size = 140 - signature.chars.length - 35 - 1 #(tinyurl: +-35, space: 1)
       max_size_truncate = max_size - "...".chars.length
       title = post.title.strip
       title = (title.chars.length > max_size ? title.chars[0...max_size_truncate] + "..." : title).to_s
-      twit.post "#{title} http://rubyonda.com/artigos/#{post.permalink}#{signature}" 
+      twit.update "#{title} http://rubyonda.com/artigos/#{post.permalink}#{signature}" 
       post.update_attribute(:tweeted_at, Time.zone.now)
     end
   end
