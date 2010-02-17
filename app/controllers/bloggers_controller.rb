59ebd464d0ac1f6f6097d8e7a99ec86605d2d2ad
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-02-17 01:23:50 -0200

    List lazy guys too

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -1,10 +1,10 @@
 class BloggersController < ApplicationController
   def index
     @bloggers = User.find(:all, {
-      :select => "count(*) as posts_count, users.*", 
-      :group => "user_id", 
-      :joins => "inner join posts on posts.user_id = users.id", 
-      :conditions => ("blogger is true"),
+      :select => "count(posts.id), users.*", 
+      :group => "email", 
+      :joins => "left outer join posts on posts.user_id = users.id", 
+      :conditions => "blogger is true",
       :order => "1 desc"
     })
   end
